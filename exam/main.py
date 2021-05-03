eb2263cf82e40c0ec6d1d68a00802dea7dd973d7
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-05-03 08:41:55 +0200

    I am ready

---
+++ exam/main.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+x = np.linspace(0, 1, 100)
+print(x)
---
+++ exam/plots.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+plt.rcParams['mathtext.fontset'] = 'cm'
+font = {'family' : 'serif', 
+        'size': 20}
+plt.rc('font', **font)
+plt.rc('lines', lw=2)
+
